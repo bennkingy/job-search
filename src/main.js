c3b8df7c708802517706df604aeda721ec6350cc
Benn King <bennkingy@gmail.com>
2022-10-23 15:57:05 +0100

    font awesome added previous commit

--- src/main.js
+++ src/main.js
@@ -11,4 +11,5 @@ import { createApp } from "vue";
 library.add(faAngleDown);
 library.add(faAngleUp);
 library.add(faSearch);
+library.add(faHome);
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
